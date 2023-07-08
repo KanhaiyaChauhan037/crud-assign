d506231813aa5aa56614c8aefaf17dad1a9c3078
kanhaiya chauhan <kanhaiyachauhan037@gmail.com>
2023-07-09 00:21:51 +0530

    crud suucess

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 const app = express();
 const usersRouter = require("./routes/users");
 app.use(express.json());
 
 const PORT = 5500;
 const mongodbUrl = "mongodb+srv://crud:crud@cluster0.fmousch.mongodb.net/";
+app.use(cors());
 app.use('/users', usersRouter);
 
 mongoose.connect(mongodbUrl)
